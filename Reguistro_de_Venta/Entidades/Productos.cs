4b33f2711fe632147819f13f9c948f4c21df375d
Erickson Fana Morales <erickson_fana@ucne.edu.do>
2021-04-21 12:40:07 -0400

    Agregando variables a la entidad de producto

--- Reguistro_de_Venta/Entidades/Productos.cs
+++ Reguistro_de_Venta/Entidades/Productos.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Reguistro_de_Venta.Entidades
 {
     class Productos
     {
+        [Key]
+        public int ProductoId { get; set; }
+        public string NombreProducto { get; set; }
+        public string MarcaProducto { get; set; }
+        public int PrecioProducto { get; set; }
+        public int CantidadProducto { get; set; }
     }
 }
