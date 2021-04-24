605db9f7a09254a151506278915a65e2b7dad31c
Erickson Fana Morales <erickson_fana@ucne.edu.do>
2021-04-23 20:59:24 -0400

    Haciendo la ventana de registro de producto

---
+++ Reguistro_de_Venta/UI/Menu/Mainform.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Reguistro_de_Venta.UI.Menu
+{
+    public partial class Mainform : Form
+    {
+        public Mainform()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Reguistro_de_Venta/UI/Registros/rProductos.Designer.cs
+++ Reguistro_de_Venta/UI/Registros/rProductos.Designer.cs
@@ -29,12 +29,147 @@ namespace Reguistro_de_Venta.UI.Registros
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.NombreProductolabel = new System.Windows.Forms.Label();
+            this.NombretextBox = new System.Windows.Forms.TextBox();
+            this.Marcalabel = new System.Windows.Forms.Label();
+            this.MarcatextBox = new System.Windows.Forms.TextBox();
+            this.Preciolabel = new System.Windows.Forms.Label();
+            this.Cantidadlabel = new System.Windows.Forms.Label();
+            this.PreciotextBox = new System.Windows.Forms.TextBox();
+            this.CantidadtextBox = new System.Windows.Forms.TextBox();
+            this.Nuevobutton = new System.Windows.Forms.Button();
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.Eliminarbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NombreProductolabel
+            // 
+            this.NombreProductolabel.AutoSize = true;
+            this.NombreProductolabel.Location = new System.Drawing.Point(39, 33);
+            this.NombreProductolabel.Name = "NombreProductolabel";
+            this.NombreProductolabel.Size = new System.Drawing.Size(64, 20);
+            this.NombreProductolabel.TabIndex = 0;
+            this.NombreProductolabel.Text = "Nombre";
+            // 
+            // NombretextBox
+            // 
+            this.NombretextBox.Location = new System.Drawing.Point(109, 30);
+            this.NombretextBox.Name = "NombretextBox";
+            this.NombretextBox.Size = new System.Drawing.Size(328, 27);
+            this.NombretextBox.TabIndex = 1;
+            // 
+            // Marcalabel
+            // 
+            this.Marcalabel.AutoSize = true;
+            this.Marcalabel.Location = new System.Drawing.Point(34, 88);
+            this.Marcalabel.Name = "Marcalabel";
+            this.Marcalabel.Size = new System.Drawing.Size(50, 20);
+            this.Marcalabel.TabIndex = 2;
+            this.Marcalabel.Text = "Marca";
+            // 
+            // MarcatextBox
+            // 
+            this.MarcatextBox.Location = new System.Drawing.Point(109, 85);
+            this.MarcatextBox.Name = "MarcatextBox";
+            this.MarcatextBox.Size = new System.Drawing.Size(328, 27);
+            this.MarcatextBox.TabIndex = 3;
+            // 
+            // Preciolabel
+            // 
+            this.Preciolabel.AutoSize = true;
+            this.Preciolabel.Location = new System.Drawing.Point(36, 135);
+            this.Preciolabel.Name = "Preciolabel";
+            this.Preciolabel.Size = new System.Drawing.Size(50, 20);
+            this.Preciolabel.TabIndex = 4;
+            this.Preciolabel.Text = "Precio";
+            // 
+            // Cantidadlabel
+            // 
+            this.Cantidadlabel.AutoSize = true;
+            this.Cantidadlabel.Location = new System.Drawing.Point(247, 135);
+            this.Cantidadlabel.Name = "Cantidadlabel";
+            this.Cantidadlabel.Size = new System.Drawing.Size(69, 20);
+            this.Cantidadlabel.TabIndex = 5;
+            this.Cantidadlabel.Text = "Cantidad";
+            // 
+            // PreciotextBox
+            // 
+            this.PreciotextBox.Location = new System.Drawing.Point(109, 135);
+            this.PreciotextBox.Name = "PreciotextBox";
+            this.PreciotextBox.Size = new System.Drawing.Size(132, 27);
+            this.PreciotextBox.TabIndex = 6;
+            // 
+            // CantidadtextBox
+            // 
+            this.CantidadtextBox.Location = new System.Drawing.Point(322, 132);
+            this.CantidadtextBox.Name = "CantidadtextBox";
+            this.CantidadtextBox.Size = new System.Drawing.Size(115, 27);
+            this.CantidadtextBox.TabIndex = 7;
+            // 
+            // Nuevobutton
+            // 
+            this.Nuevobutton.Location = new System.Drawing.Point(55, 203);
+            this.Nuevobutton.Name = "Nuevobutton";
+            this.Nuevobutton.Size = new System.Drawing.Size(94, 29);
+            this.Nuevobutton.TabIndex = 8;
+            this.Nuevobutton.Text = "Nuevo";
+            this.Nuevobutton.UseVisualStyleBackColor = true;
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.Location = new System.Drawing.Point(189, 203);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(94, 29);
+            this.Guardarbutton.TabIndex = 9;
+            this.Guardarbutton.Text = "Guardar";
+            this.Guardarbutton.UseVisualStyleBackColor = true;
+            // 
+            // Eliminarbutton
+            // 
+            this.Eliminarbutton.Location = new System.Drawing.Point(323, 203);
+            this.Eliminarbutton.Name = "Eliminarbutton";
+            this.Eliminarbutton.Size = new System.Drawing.Size(94, 29);
+            this.Eliminarbutton.TabIndex = 10;
+            this.Eliminarbutton.Text = "Eliminar";
+            this.Eliminarbutton.UseVisualStyleBackColor = true;
+            // 
+            // rProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(471, 265);
+            this.Controls.Add(this.Eliminarbutton);
+            this.Controls.Add(this.Guardarbutton);
+            this.Controls.Add(this.Nuevobutton);
+            this.Controls.Add(this.CantidadtextBox);
+            this.Controls.Add(this.PreciotextBox);
+            this.Controls.Add(this.Cantidadlabel);
+            this.Controls.Add(this.Preciolabel);
+            this.Controls.Add(this.MarcatextBox);
+            this.Controls.Add(this.Marcalabel);
+            this.Controls.Add(this.NombretextBox);
+            this.Controls.Add(this.NombreProductolabel);
+            this.Name = "rProductos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registro de Productos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label NombreProductolabel;
+        private System.Windows.Forms.TextBox NombretextBox;
+        private System.Windows.Forms.Label Marcalabel;
+        private System.Windows.Forms.TextBox MarcatextBox;
+        private System.Windows.Forms.Label Preciolabel;
+        private System.Windows.Forms.Label Cantidadlabel;
+        private System.Windows.Forms.TextBox PreciotextBox;
+        private System.Windows.Forms.TextBox CantidadtextBox;
+        private System.Windows.Forms.Button Nuevobutton;
+        private System.Windows.Forms.Button Guardarbutton;
+        private System.Windows.Forms.Button Eliminarbutton;
     }
 }
